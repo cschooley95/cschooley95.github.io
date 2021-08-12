922789d230e21285d2ba58301c1ecae85fd8d4e1
Catherine Schooley <cschooley@utah.gov>
2021-08-12 14:34:02 -0600

    Update with Time Slider

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 require([
-    "esri/WebMap",
+    "esri/Map",
     "esri/views/MapView",
-], function(WebMap, MapView) {
+    "esri/layers/FeatureLayer",
+    "esri/widgets/TimeSlider"
+  ], (Map, MapView, FeatureLayer, TimeSlider) => {
+    const layer = new FeatureLayer({
+      url:
+        "https://services.arcgis.com/ZzrwjTRez6FJiOq4/arcgis/rest/services/OGHistory/FeatureServer"
+    });
 
-    const map = new WebMap({
-        portalItem: {
-            id: "abfd3ac06bb34826a772a9f49fde3ace"
-        }
+    const map = new Map({
+      basemap: "hybrid",
+      layers: [layer]
     });
 
     const view = new MapView({
-        container: "viewDiv",
-        map: map
-    })
-})
+      map: map,
+      container: "viewDiv",
+      zoom: 4,
+      center: [-100, 30]
+    });
+
+    // time slider widget initialization
+    const timeSlider = new TimeSlider({
+      container: "timeSlider",
+      view: view,
+      timeVisible: true, // show the time stamps on the timeslider
+      loop: true
+    });
+
+    view.whenLayerView(layer).then((lv) => {
+      // around up the full time extent to full hour
+      timeSlider.fullTimeExtent = layer.timeInfo.fullTimeExtent.expandTo(
+        "years"
+      );
+      timeSlider.stops = {
+        interval: layer.timeInfo.interval
+      };
+    });
+  });
