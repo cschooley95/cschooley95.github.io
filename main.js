95ad97409e38a5ac2ba0d2b39445dffa245a01cf
Catherine Schooley <cschooley@utah.gov>
2021-08-13 09:02:56 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,15 +5,9 @@ require([
     "esri/widgets/TimeSlider"
 ], function (WebMap, MapView, TimeSlider) {
 
-  /******************************************************************
-   *
-   * Webmap example
-   *
-   ******************************************************************/
-
-  // Step 1: Pass a webmap instance to the map and specify the id for the webmap item
+    //Grabbing my web map
   const map = new WebMap({
-    portalItem: { // autocast (no need to specifically require it above)
+    portalItem: {
       id: "1e9a6937760e46d3bd047c108ebf8246"
     }
   });
@@ -22,13 +16,33 @@ require([
     container: "viewDiv",
     map: map
   });
+
+  const events = [
+    {name: "Recession of 2008", date: 2008},
+    {name: "Covid-19 Pandemic", date: 2020}
+  ];
   
+  //Creating Timeslider
   const timeSlider = new TimeSlider({
       container: "timeSlider",
       fullTimeExtent: {
           start: new Date(1900,0,1),
           end: new Date(2022,0,1)
-      }
+      },
+
+      tickConfigs: [{
+        mode: "position",
+        values: events
+          .map((event) => new Date(event.date, 0, 1))
+          .map((date) => date.getTime()),
+        labelsVisible = true,
+        labelFormatFunction: (value)=> {
+          const event = events.find(
+            (s) => new Date(s.date, 0, 1).getTime() === value
+          );
+          return event.name + ", " + event.date;
+        }
+      }]
   });
 
   view.ui.add(timeSlider)
