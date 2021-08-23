5c376feea3140f8b995726478e65cceba1cd93ed
Catherine Schooley <cschooley@utah.gov>
2021-08-23 15:22:08 -0600

    Update main.js

--- main.js
+++ main.js
@@ -130,7 +130,7 @@ timeSlider.timeExtent = {start,end};
 timeSlider.watch("timeExtent", () => {
   //oil wells that popped up before the end of the current time extent
 OGLayerView.filter = {
-  where: timeSlider.timeExtent.start.getTime() + "<= OrigComplDate <=" + timeSlider.timeExtent.end.getTime(),
+  where: timeSlider.timeExtent.start.getTime() + ">= OrigComplDate <=" + timeSlider.timeExtent.end.getTime(),
 }
 // add grayscale effect to old wells (may or may not keep this)
   OGLayerView.effect = {
